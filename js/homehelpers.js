b88d8dada3ff11d12729e699c3152a3ae9e15a64
Nick Greenfield <fatnick@gmail.com>
2024-11-25 07:30:32 +0000

    Create homehelpers.js
    Signed-off-by: Nick Greenfield <fatnick@gmail.com>

---
+++ js/homehelpers.js
@@ -0,0 +1,29 @@
+SiteArray = [];
+
+await loadsiteData("https://mechafatnick.github.io/RetroRing/Data/RetroRing.xml")
+addSites();
+function loadsiteData(url) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var x, i, xmlDoc, txt;
+      xmlDoc = this.responseXML
+      x = xmlDoc.getElementsByTagName("row")
+      for (i = 0; i< x.length; i++) {
+      SiteArray.push( {NAME: x[i].getElementsByTagName('NAME')[0].innerHTML, URL: x[i].getElementsByTagName('URL')[0].innerHTML, Description: x[i].getElementsByTagName('Description')[0].innerHTML})
+      }
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+function addSites() {
+while
+mainDiv = document.getElementById("maindiv")
+for (i = 0; i< SiteArray.length; i++) {
+siteDiv = document.createElement("div")
+siteDiv.innerHTML = '<div id="siteDiv"><a href="'+SiteArray[i].URL+'<h2 class="siteName">' + SiteArray[i].NAME +'<p class="siteDes">' + SiteArray[i].Description + '</p> </div> <p class="divider"> ---- </p>'
+mainDiv.appendChild(siteDiv)  
+}
+}
